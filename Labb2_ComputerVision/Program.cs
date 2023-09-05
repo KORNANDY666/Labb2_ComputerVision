e27d5c45316228f295049801bb5c9997619e4e45
KORNANDY666 <113092594+KORNANDY666@users.noreply.github.com>
2023-09-05 08:53:14 +0200

    change while loop

--- Labb2_ComputerVision/Program.cs
+++ Labb2_ComputerVision/Program.cs
@@ -9,24 +9,28 @@ namespace Labb2_ComputerVision
     {
         static async Task Main(string[] args)
         {
-            try
+            bool input = false; 
+
+            while (input = true)
             {
-                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
-                IConfigurationRoot configuration = builder.Build();
-                string endpoint = configuration["Endpoint"];
-                string subscriptionKey = configuration["SubscriptionKey"];
-
-                ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey))
+                try
                 {
-                    Endpoint = endpoint
-                };
+                    IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                    IConfigurationRoot configuration = builder.Build();
+                    string endpoint = configuration["Endpoint"];
+                    string subscriptionKey = configuration["SubscriptionKey"];
 
-                Console.Write("Enter the URL of the image to analyze: ");
-                string imageUrl = Console.ReadLine();
-                Console.Clear();
+                    ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey))
+                    {
+                        Endpoint = endpoint
+                    };
 
-                // Analyze the image
-                ImageAnalysis analysis = await client.AnalyzeImageAsync(imageUrl, new List<VisualFeatureTypes?>
+                    Console.Write("Enter the URL of the image you want to analyze: ");
+                    string imageUrl = Console.ReadLine();
+                    Console.Clear();
+
+                    // Analyze the image
+                    ImageAnalysis analysis = await client.AnalyzeImageAsync(imageUrl, new List<VisualFeatureTypes?>
             {
                 VisualFeatureTypes.Description,
                 VisualFeatureTypes.Categories,
@@ -38,27 +42,35 @@ namespace Labb2_ComputerVision
                 VisualFeatureTypes.Color
             });
 
-                Console.WriteLine("Tags: \n ");
-                foreach (var tag in analysis.Tags)
+                    Console.WriteLine("Tags: \n ");
+                    foreach (var tag in analysis.Tags)
+                    {
+                        Console.WriteLine($"{tag.Name}, {tag.Confidence * 100:F1}%");
+                    }
+
+                    Console.WriteLine("\n");
+                    // Process the results
+                    Console.WriteLine("Description: " + analysis.Description.Captions[0].Text);
+                    Console.WriteLine("Categories: " + string.Join(", ", analysis.Categories.Select(c => c.Name)));
+                    //Console.WriteLine("Tags: " + string.Join(", ", analysis.Tags.Select(t => t.Name)));
+                    Console.WriteLine("Adult: " + analysis.Adult.IsAdultContent);
+                    Console.WriteLine("Number of faces: " + analysis.Faces.Count);
+                    Console.WriteLine("Objects: " + string.Join(", ", analysis.Objects.Select(o => o.ObjectProperty)));
+                    Console.WriteLine("Brands: " + string.Join(", ", analysis.Brands.Select(b => b.Name)));
+                    Console.WriteLine("Is adult content: " + analysis.Adult.IsAdultContent);
+                    Console.WriteLine("Dominant colors: " + string.Join(", ", analysis.Color.DominantColors));
+                }
+                catch (Exception ex)
                 {
-                    Console.WriteLine($"{tag.Name}, {tag.Confidence * 100:F1}%");
+                    Console.WriteLine("An error occurred: " + ex.Message);
                 }
 
-                Console.WriteLine("\n");
-                // Process the results
-                Console.WriteLine("Description: " + analysis.Description.Captions[0].Text);
-                Console.WriteLine("Categories: " + string.Join(", ", analysis.Categories.Select(c => c.Name)));
-                //Console.WriteLine("Tags: " + string.Join(", ", analysis.Tags.Select(t => t.Name)));
-                Console.WriteLine("Adult: " + analysis.Adult.IsAdultContent);
-                Console.WriteLine("Number of faces: " + analysis.Faces.Count);
-                Console.WriteLine("Objects: " + string.Join(", ", analysis.Objects.Select(o => o.ObjectProperty)));
-                Console.WriteLine("Brands: " + string.Join(", ", analysis.Brands.Select(b => b.Name)));
-                Console.WriteLine("Is adult content: " + analysis.Adult.IsAdultContent);
-                Console.WriteLine("Dominant colors: " + string.Join(", ", analysis.Color.DominantColors));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                Console.WriteLine("");
+                Console.WriteLine("Do you want to chech another url image ? please enter Y/N");
+                string checkUrl = Console.ReadLine().ToLower();
+                if (checkUrl == "y") 
+                { input = true; Console.Clear(); }
+                else { input = false; break; }
             }
         }
     }
